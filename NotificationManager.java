4fb2cf7a75b513898e42cc3f2f474e4568688375
Jacopo-De-Martino <jaco.drago.tm@gmail.com>
2025-07-09 11:09:18 +0200

    Merge

---
+++ NotificationManager.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Observer;
+
+public class NotificationManager implements Subject {
+
+    private static NotificationManager istanza;
+    ArrayList<Observer> listaUtenti = new ArrayList<>();
+
+    private NotificationManager() {
+        
+    }
+
+    public static NotificationManager getInstanza() {
+        if(istanza == null) {
+            istanza = new NotificationManager();
+        }
+        return istanza;
+    }
+
+    public String inviaMessaggio(String messaggio) {
+        return messaggio;
+    }
+    
+    @Override
+    public void registerObserver(Observer o) {
+        listaUtenti.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        listaUtenti.remove(o);
+    }
+}
