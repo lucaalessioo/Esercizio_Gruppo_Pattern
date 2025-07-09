df24dee31fc6761546b6810039ca09165f138ec5
lucaalessioo <lucaalessioo@hotmail.com>
2025-07-09 11:47:32 +0200

    Aggiunti commenti

--- NotificationManager.java
+++ NotificationManager.java
@@ -1,39 +1,41 @@
 import java.util.ArrayList;
 
+// Classe singleton
 public class NotificationManager implements Subject {
 
-    private static NotificationManager istanza;
-    ArrayList<Osservatore> listaUtenti = new ArrayList<>();
+    
+    private static NotificationManager istanza;                // istanza 
+    ArrayList<Osservatore> listaUtenti = new ArrayList<>();    // Lista di osservatori
 
-    private NotificationManager() {
+    private NotificationManager() {                            // Costruttore
 
     }
 
-    public static NotificationManager getInstanza() {
+    public static NotificationManager getInstanza() {          // Metodo per creare l istanza se non è gia presente
         if (istanza == null) {
             istanza = new NotificationManager();
         }
         return istanza;
     }
 
-    public String inviaMessaggio(String messaggio) {
+    public String inviaMessaggio(String messaggio) {           // Metodo per inviare il messaggio
         return messaggio;
     }
 
     @Override
-    public void notifyOsservatore(String message) {
+    public void notifyOsservatore(String message) {            // Metodo per stampare i messaggi
         for (Osservatore o : listaUtenti) {
             o.update(message);
         }
     }
 
     @Override
-    public void registerOsservatore(Osservatore o) {
+    public void registerOsservatore(Osservatore o) {           // Metodo aggiungere gli osservatori
         listaUtenti.add(o);
     }
 
     @Override
-    public void removeOsservatore(Osservatore o) {
+    public void removeOsservatore(Osservatore o) {             // Metodo per rimuovere gli osservatori
         listaUtenti.remove(o);
     }
 }
