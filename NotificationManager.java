0972ee3ed1d87884df25670154b4b02d09fdf8a0
lucaalessioo <lucaalessioo@hotmail.com>
2025-07-09 12:08:39 +0200

    Update NotificationManager.java

--- NotificationManager.java
+++ NotificationManager.java
@@ -1,34 +1,48 @@
 import java.util.ArrayList;
-import java.util.Observer;
 
+// Classe Singleton
 public class NotificationManager implements Subject {
 
-    private static NotificationManager istanza;
-    ArrayList<Observer> listaUtenti = new ArrayList<>();
+    private static NotificationManager istanza;                                // istanza
+    private ArrayList<Osservatore> listaUtenti = new ArrayList<>();            // lista di osservatori
 
-    private NotificationManager() {
-        
+    public ArrayList<Osservatore> getListaUtenti() {                           // getter per la lista utenti
+        return listaUtenti;
     }
 
-    public static NotificationManager getInstanza() {
-        if(istanza == null) {
+    public void setListaUtenti(ArrayList<Osservatore> listaUtenti) {           // Setter per la lista utenti
+        this.listaUtenti = listaUtenti;
+    }
+
+    private NotificationManager() {                                            // Costruttore privato
+
+    }
+
+    public static NotificationManager getInstanza() {                          // Metodo per creare l istanza se non esistente
+        if (istanza == null) {
             istanza = new NotificationManager();
         }
         return istanza;
     }
 
-    public String inviaMessaggio(String messaggio) {
+    public String inviaMessaggio(String messaggio) {                           // Metodo per inviare il messaggio
         return messaggio;
-    }
-    
-    @Override
-    public void registerObserver(Observer o) {
-        listaUtenti.add(o);
     }
 
     @Override
-    public void removeObserver(Observer o) {
+    public void notifyOsservatore(String message) {                            // Metodo per vedere i messaggi
+        for (Osservatore o : listaUtenti) {
+            System.out.println(o.update(message));
+        }
+    }
+
+    @Override
+    public void registerOsservatore(Osservatore o) {                           // Metodo per aggiungere l osservatore
+        listaUtenti.add(o);
+    }
+
+    @Override   
+    public void removeOsservatore(Osservatore o) {                             // Metodo per rimuovere l osservatore
         listaUtenti.remove(o);
     }
-    
 }
