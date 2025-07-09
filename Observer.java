d2bd845ec02d6dd505bf533894a7719f0dd1294e
Spatentrider <104686281+Spatentrider@users.noreply.github.com>
2025-07-09 11:27:21 +0200

    aggiunti commenti

--- Observer.java
+++ Observer.java
@@ -1,21 +1,30 @@
+//interfaccia del Subject
 interface Subject{
+    //metodo per aggiungere Observer
     void registerObserver(Observer o);
+    //metodo per rimuovere Observer
     void removeObserver(Observer o);
+    //metodo per notificare Observer
     void notifyObservers(String message);
 }
 
+//interfaccia dell?Observer
 interface Observer{
     String update(String message);
 }
 
+//classe Observer che implementa interfaccia Observer
 class ConcreteObserver implements Observer{
 
+    //attributo privato del nome
     private String name;
 
+    //costruttore con parametro
     public ConcreteObserver(String name){
         this.name = name;
     }
  
+    //Override del metodo update con messaggio
     @Override
     public String update(String message) {
         return name + " ha ricevuto aggiornamento: " + message;
